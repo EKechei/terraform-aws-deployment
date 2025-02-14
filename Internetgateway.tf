d6c9075cf8fd67d35e3e15e0caa040a6fb3c0b3a
root <root@DESKTOP-3L2MJGH>
2025-02-14 18:26:52 +0300

    new files

---
+++ Internetgateway.tf
@@ -0,0 +1,25 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table_association" "RT_association" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.RT.id
+}
